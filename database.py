4b213eb889a4ae5c7b2634dc824b119ddfd4ab18
Gkisi27 <gkisi2772@gmail.com>
2023-03-10 14:28:01 +0545

    MySQL connected with students, books and issues.

--- database.py
+++ database.py
@@ -21,6 +21,7 @@ class ConnectToMySQL():
             database = self.database
         )
 
+    #Teachers Page
     def get_teachers_data_from_db(self):
         try:
             self.connect()
@@ -49,3 +50,94 @@ class ConnectToMySQL():
             if self.conn:
                 self.conn.commit()
                 self.conn.close()
+
+    #Students Page
+    def get_students_data_from_db(self):
+        try:
+            self.connect()
+            cursor = self.conn.cursor(dictionary = True)
+            sql = "SELECT * FROM students"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return result
+        except Exception as e:
+            print("Failed to get data.")
+            print(e)
+        finally:
+            if self.conn:
+                self.conn.close()
+
+    def add_student_data_to_db(self,first,last,batch,faculty,year,semester,rollno,contact,address):
+        try:
+            self.connect()
+            cursor = self.conn.cursor(dictionary = True)
+            sql = f"INSERT INTO students (firstname, lastname, batch, faculty, year, semester, rollno, contact, address) VALUES ('{first}', '{last}', '{batch}', '{faculty}', '{year}', '{semester}', '{rollno}', '{contact}', '{address}')"
+            cursor.execute(sql)
+        except Exception as e:
+            print("Failed to insert data.")
+            print(e)
+        finally:
+            if self.conn:
+                self.conn.commit()
+                self.conn.close()
+
+   #Books Page
+    def get_books_data_from_db(self):
+        try:
+            self.connect()
+            cursor = self.conn.cursor(dictionary = True)
+            sql = "SELECT * FROM books"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return result
+        except Exception as e:
+            print("Failed to get data.")
+            print(e)
+        finally:
+            if self.conn:
+                self.conn.close()
+
+    def add_book_data_to_db(self,isbn,title,author,publisher,category,quantity):
+        try:
+            self.connect()
+            cursor = self.conn.cursor(dictionary = True)
+            sql = f"INSERT INTO books (isbn, title, author, publisher, category, quantity) VALUES ('{isbn}', '{title}', '{author}', '{publisher}', '{category}', '{quantity}')"
+            cursor.execute(sql)
+        except Exception as e:
+            print("Failed to insert data.")
+            print(e)
+        finally:
+            if self.conn:
+                self.conn.commit()
+                self.conn.close()
+
+   #Issues Page
+    def get_issues_data_from_db(self):
+        try:
+            self.connect()
+            cursor = self.conn.cursor(dictionary = True)
+            sql = "SELECT * FROM issues"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return result
+        except Exception as e:
+            print("Failed to get data.")
+            print(e)
+        finally:
+            if self.conn:
+                self.conn.close()
+
+    def add_issue_data_to_db(self,book_no, student_id, issue_date, due_date, return_date, fine):
+        try:
+            self.connect()
+            cursor = self.conn.cursor(dictionary = True)
+            sql = f"INSERT INTO issues (book_no, student_id, issue_date, due_date, return_date, fine) VALUES ('{book_no}', '{student_id}', '{issue_date}', '{due_date}', '{return_date}', '{fine}')"
+            cursor.execute(sql)
+        except Exception as e:
+            print("Failed to insert data.")
+            print(e)
+        finally:
+            if self.conn:
+                self.conn.commit()
+                self.conn.close()
+
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import sys
-from PyQt5 import QtWidgets
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-from matplotlib.figure import Figure
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTableWidgetItem, QMessageBox, QVBoxLayout
 from PyQt5.QtCore import pyqtSlot
-import pandas as pd
-import numpy as np
+from datetime import datetime
 
 from library_management import Ui_MainWindow
 from database import ConnectToMySQL
@@ -37,6 +34,9 @@ class MainWindow(QMainWindow):
         self.ui.settings_btn_1.clicked.connect(self.on_settings_button_clicked)
         self.ui.settings_btn_2.clicked.connect(self.on_settings_button_clicked)
         self.ui.add_teacher_btn.clicked.connect(self.add_new_teacher)
+        self.ui.add_student_btn.clicked.connect(self.add_new_student)
+        self.ui.add_book_btn.clicked.connect(self.add_new_book)
+        self.ui.add_issue_btn.clicked.connect(self.add_new_issue)
         self.ui.login_btn.clicked.connect(self.login_method)
 
         #Plotting current data in dashboard
@@ -99,16 +99,34 @@ class MainWindow(QMainWindow):
             QMessageBox.information(self, 'Warning', 'You are not logged in.')
 
     def on_students_button_clicked(self):
-        self.ui.LMS_label.setText("Students")
-        self.ui.stackedWidget.setCurrentWidget(self.ui.students_page)
+        if self.logged_in == True:
+            self.ui.LMS_label.setText("Students")
+            self.ui.stackedWidget.setCurrentWidget(self.ui.students_page)
+            self.get_students_data()
+        else:
+            self.ui.LMS_label.setText("Login")
+            self.ui.stackedWidget.setCurrentWidget(self.ui.login_page)
+            QMessageBox.information(self, 'Warning', 'You are not logged in.')
 
     def on_books_button_clicked(self):
-        self.ui.LMS_label.setText("Books")
-        self.ui.stackedWidget.setCurrentWidget(self.ui.books_page)
+        if self.logged_in == True:
+            self.ui.LMS_label.setText("Books")
+            self.ui.stackedWidget.setCurrentWidget(self.ui.books_page)
+            self.get_books_data()
+        else:
+            self.ui.LMS_label.setText("Login")
+            self.ui.stackedWidget.setCurrentWidget(self.ui.login_page)
+            QMessageBox.information(self, 'Warning', 'You are not logged in.')
 
     def on_issues_button_clicked(self):
-        self.ui.LMS_label.setText("Isssues")
-        self.ui.stackedWidget.setCurrentWidget(self.ui.issues_page)
+        if self.logged_in == True:
+            self.ui.LMS_label.setText("Issues")
+            self.ui.stackedWidget.setCurrentWidget(self.ui.issues_page)
+            self.get_issues_data()
+        else:
+            self.ui.LMS_label.setText("Login")
+            self.ui.stackedWidget.setCurrentWidget(self.ui.login_page)
+            QMessageBox.information(self, 'Warning', 'You are not logged in.')
 
     def on_settings_button_clicked(self):
         self.ui.LMS_label.setText("Settings")
@@ -130,6 +148,7 @@ class MainWindow(QMainWindow):
                 t_contact = QTableWidgetItem(str(item['contact']))
                 t_email = QTableWidgetItem(str(item['email']))
                 t_address = QTableWidgetItem(str(item['address']))
+                t_joined_date = QTableWidgetItem(str(item['joineddate']))
 
                 self.ui.teacher_table_widget.setItem(row, 0, t_firstname)
                 self.ui.teacher_table_widget.setItem(row, 1, t_lastname)
@@ -137,6 +156,7 @@ class MainWindow(QMainWindow):
                 self.ui.teacher_table_widget.setItem(row, 3, t_contact)
                 self.ui.teacher_table_widget.setItem(row, 4, t_email)
                 self.ui.teacher_table_widget.setItem(row, 5, t_address)
+                self.ui.teacher_table_widget.setItem(row, 6, t_joined_date)
 
         else:
             QMessageBox.information(self, 'Warning', 'Data could not be loaded. Please try again.')
@@ -166,11 +186,186 @@ class MainWindow(QMainWindow):
         if t_address == "":
             QMessageBox.information(self, 'Warning', 'Does this teacher lives in space?')
             return None
+        t_joined_date = str(datetime.today().year)
 
-        ConnectToMySQL().add_teacher_data_to_db(t_first,t_last,t_subject,t_contact,t_email,t_address)
+        ConnectToMySQL().add_teacher_data_to_db(t_first, t_last, t_subject, t_contact, t_email, t_address, t_joined_date)
         self.get_teachers_data()
 
 
+    # Students Page
+    @pyqtSlot(bool)
+    def get_students_data(self):
+        result = ConnectToMySQL().get_students_data_from_db()
+        if result:
+            self.ui.student_table_widget.setRowCount(len(result))
+            for row,item in enumerate(result):
+                s_firstname = QTableWidgetItem(str(item['firstname']))
+                s_lastname = QTableWidgetItem(str(item['lastname']))
+                s_batch = QTableWidgetItem(str(item['batch']))
+                s_faculty = QTableWidgetItem(str(item['faculty']))
+                s_year = QTableWidgetItem(str(item['year']))
+                s_semester = QTableWidgetItem(str(item['semester']))
+                s_rollno = QTableWidgetItem(str(item['rollno']))
+                s_contact = QTableWidgetItem(str(item['contact']))
+                s_address = QTableWidgetItem(str(item['address']))
+
+                self.ui.student_table_widget.setItem(row, 0, s_firstname)
+                self.ui.student_table_widget.setItem(row, 1, s_lastname)
+                self.ui.student_table_widget.setItem(row, 2, s_batch)
+                self.ui.student_table_widget.setItem(row, 3, s_faculty)
+                self.ui.student_table_widget.setItem(row, 4, s_year)
+                self.ui.student_table_widget.setItem(row, 5, s_semester)
+                self.ui.student_table_widget.setItem(row, 6, s_rollno)
+                self.ui.student_table_widget.setItem(row, 7, s_contact)
+                self.ui.student_table_widget.setItem(row, 8, s_address)
+
+        else:
+            QMessageBox.information(self, 'Warning', 'Data could not be loaded. Please try again.')
+
+    def add_new_student(self):
+        s_first = self.ui.student_first_name.text()
+        if s_first == "":
+            QMessageBox.information(self, 'Warning', 'Firstname cannot be empty.')
+            return None
+        s_last = self.ui.student_last_name.text()
+        if s_last == "":
+            QMessageBox.information(self, 'Warning', 'Lastname cannot be empty.')
+            return None
+        s_batch = self.ui.student_batch.text()
+        if s_batch == "":
+            QMessageBox.information(self, 'Warning', 'Batch cannot be empty.')
+            return None
+        s_faculty = self.ui.student_faculty.text()
+        if s_faculty == "":
+            QMessageBox.information(self, 'Warning', 'Faculty cannot be empty.')
+            return None
+        s_year = self.ui.student_year.text()
+        if s_year == "":
+            QMessageBox.information(self, 'Warning', 'Year cannot be empty.')
+            return None
+        s_semester = self.ui.student_semester.text()
+        if s_semester == "":
+            QMessageBox.information(self, 'Warning', 'Semester cannot be empty.')
+            return None
+        s_rollno = self.ui.student_roll_no.text()
+        if s_rollno == "":
+            QMessageBox.information(self, 'Warning', 'Roll No cannot be empty.')
+            return None
+        s_contact = self.ui.student_contact.text()
+        if s_contact == "":
+            QMessageBox.information(self, 'Warning', 'Contact cannot be empty.')
+            return None        
+        s_address = self.ui.student_address.text()
+        if s_address == "":
+            QMessageBox.information(self, 'Warning', 'Address cannot be empty.')
+            return None
+
+        ConnectToMySQL().add_student_data_to_db(s_first, s_last, s_batch, s_faculty, s_year, s_semester, s_rollno, s_contact, s_address)
+        self.get_students_data()
+
+    # Books Page
+    @pyqtSlot(bool)
+    def get_books_data(self):
+        result = ConnectToMySQL().get_books_data_from_db()
+        if result:
+            self.ui.book_table_widget.setRowCount(len(result))
+            for row,item in enumerate(result):
+                b_isbn = QTableWidgetItem(str(item['isbn']))
+                b_title = QTableWidgetItem(str(item['title']))
+                b_author = QTableWidgetItem(str(item['author']))
+                b_publisher = QTableWidgetItem(str(item['publisher']))
+                b_category = QTableWidgetItem(str(item['category']))
+                b_quantity = QTableWidgetItem(str(item['quantity']))
+
+                self.ui.book_table_widget.setItem(row, 0, b_isbn)
+                self.ui.book_table_widget.setItem(row, 1, b_title)
+                self.ui.book_table_widget.setItem(row, 2, b_author)
+                self.ui.book_table_widget.setItem(row, 3, b_publisher)
+                self.ui.book_table_widget.setItem(row, 4, b_category)
+                self.ui.book_table_widget.setItem(row, 5, b_quantity)
+
+
+        else:
+            QMessageBox.information(self, 'Warning', 'Data could not be loaded. Please try again.')
+
+    def add_new_book(self):
+        b_isbn = self.ui.book_isbn.text()
+        if b_isbn == "":
+            QMessageBox.information(self, 'Warning', 'ISBN cannot be empty.')
+            return None
+        b_title = self.ui.book_title.text()
+        if b_title == "":
+            QMessageBox.information(self, 'Warning', 'Title cannot be empty.')
+            return None
+        b_author = self.ui.book_author.text()
+        if b_author == "":
+            QMessageBox.information(self, 'Warning', 'Author cannot be empty.')
+            return None
+        b_publisher = self.ui.book_publisher.text()
+        if b_publisher == "":
+            QMessageBox.information(self, 'Warning', 'Publisher cannot be empty.')
+            return None
+        b_category = self.ui.book_category.text()
+        if b_category == "":
+            QMessageBox.information(self, 'Warning', 'Category cannot be empty.')
+            return None
+        b_quantity = self.ui.book_quantity.text()
+        if b_quantity == "":
+            QMessageBox.information(self, 'Warning', 'Quantity cannot be empty.')
+            return None
+
+        ConnectToMySQL().add_book_data_to_db(b_isbn, b_title, b_author, b_publisher, b_category, b_quantity)
+        self.get_books_data()
+
+    # Issues Page
+    @pyqtSlot(bool)
+    def get_issues_data(self):
+        result = ConnectToMySQL().get_issues_data_from_db()
+        if result:
+            self.ui.issue_table_widget.setRowCount(len(result))
+            for row,item in enumerate(result):
+                i_book_no = QTableWidgetItem(str(item['book_no']))
+                i_student_id = QTableWidgetItem(str(item['student_id']))
+                i_issue_date = QTableWidgetItem(str(item['issue_date']))
+                i_due_date = QTableWidgetItem(str(item['due_date']))
+                i_return_date = QTableWidgetItem(str(item['return_date']))
+                i_fine = QTableWidgetItem(str(item['fine']))
+
+                self.ui.issue_table_widget.setItem(row, 0, i_book_no)
+                self.ui.issue_table_widget.setItem(row, 1, i_student_id)
+                self.ui.issue_table_widget.setItem(row, 2, i_issue_date)
+                self.ui.issue_table_widget.setItem(row, 3, i_due_date)
+                self.ui.issue_table_widget.setItem(row, 4, i_return_date)
+                self.ui.issue_table_widget.setItem(row, 5, i_fine)
+
+        else:
+            QMessageBox.information(self, 'Warning', 'Data could not be loaded. Please try again.')
+
+    def add_new_issue(self):
+        i_book_no = self.ui.issue_book_no.text()
+        if i_book_no == "":
+            QMessageBox.information(self, 'Warning', 'Book No cannot be empty.')
+            return None
+        i_student_id = self.ui.issue_student_id.text()
+        if i_student_id == "":
+            QMessageBox.information(self, 'Warning', 'Student ID cannot be empty.')
+            return None
+        i_issue_date = self.ui.issue_date.text()
+        if i_issue_date == "":
+            QMessageBox.information(self, 'Warning', 'Issue date cannot be empty.')
+            return None
+        i_due_date = self.ui.issue_due_date.text()
+        if i_due_date == "":
+            QMessageBox.information(self, 'Warning', 'Due date cannot be empty.')
+            return None
+        i_return_date = self.ui.issue_return_date.text()
+        if i_return_date == "":
+            QMessageBox.information(self, 'Warning', 'Return date cannot be empty.')
+            return None
+        i_fine = self.ui.issue_address.text()
+
+        ConnectToMySQL().add_teacher_data_to_db(i_book_no, i_student_id, i_issue_date, i_due_date, i_return_date, i_fine)
+        self.get_teachers_data()
 
 
 if __name__ == "__main__":
